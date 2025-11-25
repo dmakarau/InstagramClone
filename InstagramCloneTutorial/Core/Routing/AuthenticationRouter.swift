c419dcd06b5f27046098da16d2362ac3d5e50711
dmakarau <denis.makarow@gmail.com>
2025-11-25 22:31:22 +0100

    refactor(auth): use AuthenticationRouter for step-driven registration

---
+++ InstagramCloneTutorial/Core/Routing/AuthenticationRouter.swift
@@ -0,0 +1,8 @@
+//
+//  RegistrationRouter.swift
+//  InstagramCloneTutorial
+//
+//  Created by Denis Makarau on 17.11.25.
+//
+
+import Foundation
---
+++ InstagramCloneTutorial/Core/Routing/RegistrationSteps.swift
@@ -0,0 +1,8 @@
+//
+//  RegistrationSteps.swift
+//  InstagramCloneTutorial
+//
+//  Created by Denis Makarau on 17.11.25.
+//
+
+import Foundation
